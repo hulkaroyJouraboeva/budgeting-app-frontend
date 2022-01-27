c55b877f261fee765e63fa3d19f08358be051850
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-01-26 20:40:06 -0500

    testtttttt

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -14,6 +14,7 @@ export default function Transactions() {
   // }, [API]);
 
   useEffect(() => {
+    console.log('useEffect is running!');
     const fetchData = async () => {
       const response = await axios.get(`${API}/transactions`);
       console.log(response)
